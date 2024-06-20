a92ed7298d965616f666169dc29ffbf08d3f3a4e
Suhail <Suhail@binghzal.com>
2024-06-20 17:25:10 +0400

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Suhail Alblooshi on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
